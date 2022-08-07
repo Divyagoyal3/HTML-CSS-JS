6b9dddcebb767202aa8d58b9b02353775f4d489b
Divya Goyal <goyaldivya631@gmail.com>
2022-08-07 14:53:51 +0530

    day1

---
+++ js-basics/js-basics/script.js
@@ -0,0 +1,152 @@
+//variable
+
+// var
+//let
+//const
+//declare multiple variables
+//var age = 100,
+//city = "Manndi Adampur",
+//ismarried = false;
+//------
+//Rules of js
+//--------
+//1. Case Sensitive
+//2. Statement must be end with semicolon
+//3.Whitw space are ignored
+//4. Comments are ignored
+let myvariable = "garima goyal";
+console.log(myvariable);
+
+let $name = "Divya Goyal";
+
+let first_name = "divya";
+
+let Lastname = "goyal";
+
+let are_you_married = true;
+
+// data types primitive and object types
+
+// 1. undefined
+
+let age = undefined;
+
+// 2. null
+
+let fname = null;
+
+//3 . boolean
+let ismarried = false;
+
+//4. number
+let qty = 40;
+
+//5. string
+let lname = "doee";
+
+//console.log("js connected");
+
+// operators
+let sum = 87 + 45;
+
+//addition
+let num1 = 45;
+let num2 = 87;
+let sum1 = num1 + num2;
+//console.log(sum1);
+
+// subtraction
+let diff = num1 - num2;
+//console.log(diff);
+
+//division
+let div = num1 / num2;
+
+// modulus
+let mod = num2 / num1;
+
+// incremnet
+// 1. Post increment
+// 2. Prefix increment
+
+//1. postfix increment
+let x = 6;
+let y = x++;
+
+//2. prefix increment
+let m = 19;
+let z = ++m;
+
+// 3. postfix decrement
+let a = 8;
+let b = a--;
+
+// 4. prefix decrement
+let w = 34;
+let h = --w;
+
+// assignemnt operator
+// =
+// +=
+//-=
+// /=
+
+//=
+let city = "Chandigarh";
+
+//+=
+let u = 10;
+u += 70;
+
+// -=
+let f = 10;
+f -= 5;
+
+//===== Diff between single(=) , double(==) , triple equal to
+//(===)
+
+2 == 2;
+
+// compare string to a number
+5 == "hi"; // false value
+
+// true value it does'nt take care of data types, it care of number
+5 == "5";
+
+// triple
+// take care of value and data types both
+7 === "5"; // return false value
+"di" === "di"; // return true value
+
+// comp
+let k = 2 == 2; // return true
+let l = 9 === "9"; // return false
+
+// comparsion operators
+
+// 1. greater than
+//2. less than
+// greater then equal to
+// less than equal to
+
+let result = 2 < 1; // false
+
+let fi = 4 <= 9; // true
+
+// logical operatrs
+
+// and &&
+// or ||
+// not !
+let fullname = undefined;
+let ages = 20;
+let marry = false;
+
+// and
+let res = fullname && ages;
+
+// or
+let res1 = fullname || ages;
+
+// not!
+let res3 = !marry;
