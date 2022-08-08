779a823961d84fe511b7972a19a2c69e1b57e2cb
Divya Goyal <goyaldivya631@gmail.com>
2022-08-08 14:01:15 +0530

    day2js

---
+++ js-basics/js-basics/loops.js
@@ -0,0 +1,30 @@
+//conditional statements
+/*let totalscore = 0.8;
+let studentscore = 0.85;
+//f (12 > umr) {
+// console.log("your are eligible to vote");
+//} else {
+//console.log("not eligible");
+//
+//if (studentscore > totalscore) {
+  //console.log("you are qualified for scholarship");
+//} else {
+  //console.log("not applicable for scholarship");
+//*/
+/* truthy and falsy values
+1. falsy
+2. "" 
+3. undefined
+4. null
+
+truthy values*/
+/*let umar = true;
+if (umar) {
+  console.log("yes");
+} else {
+//   console.log("no soory");
+}*/
+// // let i;
+// for (i = 0; i < 5; i++) {
+//   console.log(i);
+// }
